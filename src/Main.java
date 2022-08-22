94e49e4b01d6a44244d9f5fb79735b3ba28b1488
homework2 <amosovaoksana16@gmail.com>
2022-08-22 23:58:41 +0500

    коммит 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int dog = 7;
+        byte cat = 1;
+        short mouse = 3;
+        long elephant = 110L;
+        double sugarWeight = 4.5;
+        float saltWeight = 1.5f;
+        boolean dogIsAdult = dog > 10;
+        char bar = 35;
+        System.out.println (dogIsAdult);
+
+        // Задание 2
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double weightOfAllBoxers = boxer1 + boxer2;
+        double weightDifference = boxer2 - boxer1;
+        System.out.println("Общий вес боксеров" + weightOfAllBoxers);
+        System.out.println("Разница в весе боксеров" + weightDifference);
+
+        // Задание 3
+
+
     }
+
 }
+
+
+
+
+
